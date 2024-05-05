8f9d17061a3f07b4e75c6dc587c2dbce5a684ce7
AlashwalQ <157615549+AlashwalQ@users.noreply.github.com>
2024-05-05 17:46:30 +0300

    I add 3 Q - test it guys
    bbecause there is an issue in my vs code with prisma files

---
+++ web_dev_phase2/app/statsPage/page.jsx
@@ -0,0 +1,18 @@
+import * as ItemsRepo from "../repo/ItemsRepo";
+
+
+export default async function Home() {
+    // const totalPurchasesPerItemPerYear = await ItemsRepo.getTotalPurchasesPerItemPerYear();
+    const top3ItemsLastSixMonths = await ItemsRepo.getTop3ItemsLastSixMonths();
+    return(<main>
+        {/* <h1>{totalPurchasesPerItemPerYear}</h1> */}
+        {top3ItemsLastSixMonths.map((item) => (
+        <div key={item.itemId}>
+          <h2>{item.itemId}</h2>
+          <p>Date : {item.dateOfPurchase}</p>
+        </div>
+      ))}
+
+    </main>);
+}
+  
