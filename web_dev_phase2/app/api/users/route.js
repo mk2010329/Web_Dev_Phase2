052ab0a7ed86e2f9a881f2ab4a9a2f3c36e90f3a
mohammad-mk2010329 <hassamtk@gmail.com>
2024-05-11 22:34:39 +0300

    removed unnecessary things

--- web_dev_phase2/app/api/users/route.js
+++
@@ -1,8 +0,0 @@
-import ItemsRepo from "@/app/repo/UsersRepo";
-// const accountsRepo = new AccountsRepo();
-
-export async function GET(request){
-    const Items = await ItemsRepo.getItems();
-    console.log(Response.json(Items));
-    return Response.json(Items,{status:200});
-}
